a276943b022363d0e2a3d4a590648d3670ba7554
IrinaBurtovaya <burtovaya.iv@gmail.com>
2022-05-18 16:57:28 +0300

    qa.guru-hm13-smallProject added

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,50 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
+import helpers.Attachments;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class TestBase {
+
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+    static String selenoid = System.getProperty("selenoid", "selenoid.autotests.cloud/wd/hub");
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://" + config.login() + ":" + config.password() + "@" + selenoid;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void openPage() {
+        step("Открываем страницу для тестирования", () -> {
+            open("https://qa.guru/");
+        });
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+        closeWebDriver();
+    }
+}
+
